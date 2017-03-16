a466b3a7102483cbf9771eccb084c8c2f0ae4616
Anthony Gubler <agubler@sitepen.com>
2017-03-16 17:18:43 +0000

    demo-2

--- src/widgets/HelloWorld.ts
+++ src/widgets/HelloWorld.ts
@@ -1,9 +1,15 @@
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 import { WidgetProperties } from '@dojo/widget-core/interfaces';
 import { v } from '@dojo/widget-core/d';
+import { ThemeableMixin, theme } from '@dojo/widget-core/mixins/Themeable';
 
-export default class HelloWorld extends WidgetBase<WidgetProperties> {
+import * as css from './styles/HelloWorld.css';
+
+const HelloWorldBase = ThemeableMixin(WidgetBase);
+
+@theme(css)
+export default class HelloWorld extends HelloWorldBase<WidgetProperties> {
 	render() {
-		return v('div', [ 'Hello, Dojo World!' ]);
+		return v('div', { classes: this.classes(css.hello) }, [ 'Hello, Dojo World!' ]);
 	}
 }
