6ca1f2353edc50cad98f51b956fda2f25bccb49b
Anthony Gubler <agubler@sitepen.com>
2017-03-16 17:24:31 +0000

    demo-3

--- src/App.ts
+++ src/App.ts
@@ -2,10 +2,15 @@ import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 import { DNode } from '@dojo/widget-core/interfaces';
 import { w } from '@dojo/widget-core/d';
 
-import HelloWorld from './widgets/HelloWorld';
+import HelloWorld, { HelloWorldProperties } from './widgets/HelloWorld';
 
 export default class App extends WidgetBase<any> {
+
+	private stranger: boolean = true;
+
 	render(): DNode {
-		return w(HelloWorld, {});
+		const { stranger } = this;
+
+		return w<HelloWorldProperties>(HelloWorld, { stranger });
 	}
 }
